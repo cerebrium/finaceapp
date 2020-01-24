14793a0092890c35e1a226ec6fa158ce365d706c
cerebrium <nicholas.m.shankland@gmail.com>
2020-01-23 18:07:43 -0800

    working on grabbing movies

--- client/src/welcomePage.jsx
+++ client/src/welcomePage.jsx
@@ -16,8 +16,11 @@ const propTypes = {
 const WelcomePage = (props, transcript, resetTranscript, startListening, browserSupportsSpeechRecognition) => {
     const [ isBlocked, setIsBlocked ] = useState(false)
     const [ record, setRecord] = useState(false) 
+    const [ amazonMovies, setAmazonMovies ] = useState([])
+    const [ netflixMovies, setNetflixMovies ] = useState([])
+
+    // Defining the outer speechrecognition container
     const recognition = new SpeechRecognition()
-    console.log(recognition)
 
       // logout
     const logout = () => {
@@ -32,32 +35,29 @@ const WelcomePage = (props, transcript, resetTranscript, startListening, browser
         setRecord(true)
         props.resetTranscript()
         props.startListening()
-        console.log(props)
     }
     
     // Stop Recording
-    const stopRecording = () => {
+    const stopRecording = (ev) => {
+        ev.preventDefault()
         setRecord(false)
         props.stopListening()
     }
 
     // Real Time blob recording in process
     const onData = (recordedBlob) => {
-        console.log('chunk of real-time data is: ', recordedBlob);
+        console.log('');
     }
  
     // If blob is done recording here it is
     const onStop = (recordedBlob) => {
-        console.log(recordedBlob)
-        axios.post('/api/speech', {
-            blobURL: recordedBlob.blobURL
-        }).then ( response => {
-            console.log(response.data)
-        })
+        console.log('')
     }
 
     // Check if the user is allowing use of the comuter mic
     useEffect(() => {
+        setNetflixMovies(['netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', 'netflixMovie', ])
+        setAmazonMovies(['amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', 'amazonMovie', ])
         navigator.mediaDevices.getUserMedia({audio: true})
         .then( (info) => {
             if (info.active) {
@@ -68,7 +68,35 @@ const WelcomePage = (props, transcript, resetTranscript, startListening, browser
             console.log(err)
         });
     }, [])
+
+
+    // list all the movies that fit the criteria for netflix
+    const handleNetflixClick = () => {
+        let mappedNetflixMovies = netflixMovies.map((ele, index) => 
+            <h3 key={index}>{ele}</h3>
+        )
+
+        movieList = (
+            <>
+                {mappedNetflixMovies}
+            </>
+        )
+    }
     
+    // list all the movies that fit the criteria for amazon
+    const handleAmazonClick = () => {
+        let mappedAmazonMovies = amazonMovies.map((ele, index) => 
+        <h3 key={index}>{ele}</h3>
+        )
+        
+        movieList = (
+            <>
+                {mappedAmazonMovies}
+            </>
+        )
+    }
+
+    console.log(movieList)
     return (
         <>
             <nav className='navbar'>
@@ -76,17 +104,23 @@ const WelcomePage = (props, transcript, resetTranscript, startListening, browser
                 <Link to='/link1' className='links'>link1</Link>{' | '}
                 <Link to='/link2' className='links'>link2</Link>{' | '}
             </nav>
-            <div >
-                <h1>Welcome Home</h1>
-                <div className='welcomePage'>
-                    <ReactMic record={record} className="sound-wave" onStop={onStop} onData={onData}/>
-                    <span>{props.transcript}</span>
-                    <button onClick={startRecording}>
-                        <h3>Record</h3>
-                    </button>
-                    <button onClick={stopRecording}>
-                        <h3>Stop</h3>
-                    </button>
+            <div className='welcomePage'>
+                <div className='positionThis'>
+                    <h1>Speak to search movies</h1>
+                    <ReactMic record={record} className="sound-wave" onStop={onStop} onData={onData} className='reactMic' opacity='.6' backgroundColor='rgb(235, 231, 231)'/>
+                    <h2>{props.transcript}</h2>
+                    <div className='nonColumn'>
+                        <button onClick={startRecording}>
+                            <h3>Record</h3>
+                        </button>
+                        <button onClick={stopRecording}>
+                            <h3>Stop</h3>
+                        </button>
+                    </div>
+                </div>
+                <div className='positionThisTwo'>
+                    <nav className='centerThis'><h2 onClick={handleNetflixClick}>Netflix</h2> <h2>|</h2> <h2 onClick={handleAmazonClick}>Amazon</h2></nav>
+                        {movieList}
                 </div>
             </div>
         </>
